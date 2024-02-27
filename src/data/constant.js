fd92d5402266da3dd5575125d14758cb9180df20
veeravelS <sveeravel16@gmail.com>
2024-02-27 15:26:04 +0530

    update

--- src/data/constant.js
+++ src/data/constant.js
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/veeravelS",
   resume:
-    "https://drive.google.com/file/d/1PllQwPXUn8bG4ZuNy-Opf_h_8G_lz6tr/view?usp=sharing",
+    "https://drive.google.com/file/d/1D5zN3fCvCGIrBDWiqcEagy_Ee3EKN1rO/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/veeravel",
   email:"mailto:sveerave16@gmail.com",
   phone:"tel:+91 6369441531",
