273817b17451de8884407a792ea28e7ee832767c
Veeravel-S <veeravel.igt@gmail.com>
2025-10-05 13:32:54 +0530

    Add logo images for Next.js, Node.js, and TypeScript
    - Added nextjs_logo.jpg to the public directory.
    - Added nodejs_logo.jpg to the public directory.
    - Added typescript_logo.png to the public directory.

--- src/data/constant.js
+++ src/data/constant.js
@@ -1,14 +1,14 @@
 export const Bio = {
   name: "Veeravel",
-  roles: ["Front End Developer", "React JS Developer", "Javascript Developer"],
+  roles: ["Front End Developer", "React JS Developer", "Javascript Developer", "Full Stack Developer"],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/veeravelS",
   resume:
     "https://drive.google.com/file/d/1g7iGUC2E3gQtpFAbl6yeCU385GOOkH7i/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/veeravel",
-  email:"mailto:sveerave16@gmail.com",
-  phone:"tel:+91 6369441531",
+  email: "mailto:sveerave16@gmail.com",
+  phone: "tel:+91 6369441531",
 };
 
 export const skills = [
@@ -45,6 +45,14 @@ export const skills = [
           "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
       },
       {
+        name: "Typescript",
+        image: "/typescript_logo.png",
+      },
+      {
+        name: "Next JS",
+        image: "/nextjs_logo.jpg",
+      },
+      {
         name: "Bootstrap",
         image:
           "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
@@ -53,14 +61,44 @@ export const skills = [
         name: "Tailwind CSS",
         image:
           "https://img.icons8.com/?size=100&id=x7XMNGh2vdqA&format=png&color=000000",
-      },{
-        name:"Shadcn UI",
-        image:"https://avatars.githubusercontent.com/u/139895814?v=4"
       },
       {
-         name:"Ant Design",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8ZtFyi4pRFo-xHoc30sSFzYVAzXc2lNKkmlhYnLPGBdh32UeiDFnhxsfereVqRpfEaf8&usqp=CAU"
-      }
+        name: "Shadcn UI",
+        image: "https://avatars.githubusercontent.com/u/139895814?v=4",
+      },
+      {
+        name: "Ant Design",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8ZtFyi4pRFo-xHoc30sSFzYVAzXc2lNKkmlhYnLPGBdh32UeiDFnhxsfereVqRpfEaf8&usqp=CAU",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node JS",
+        image:
+          "/nodejs_logo.jpg",
+      },
+      {
+        name: "Express JS",
+        image:
+          "/expressjs_logo.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "/mysql_logo.png",
+      },
+      {
+        name: "MongoDB",
+        image: "/mongodb_logo.png",
+      },
+            {
+        name: "Socket.io",
+        image: "https://socket.io/images/logo-dark.svg",
+      },
     ],
   },
   {
@@ -68,8 +106,7 @@ export const skills = [
     skills: [
       {
         name: "Git",
-        image:
-          "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+        image: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
       },
       {
         name: "GitHub",
@@ -82,8 +119,9 @@ export const skills = [
           "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
       },
       {
-        name:"Vercel",
-        image:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAAaVBMVEUAAAD///8FBQXw8PBiYmLY2Nj19fX6+vqmpqbm5ubr6+sXFxc9PT3u7u5sbGyPj4+Xl5e5ubnAwMBISEh5eXkhISFOTk7MzMxYWFiIiIgpKSmxsbE1NTXV1dVnZ2eUlJR1dXUwMDCenp4qOBGAAAAB5UlEQVR4nO3b2VLCQBSE4RwkKAZBQFRcEH3/h7TciTnZtMo+cf7vCbr6aqYnyTIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAjnymTjAcY8vVEYbjzGyuzjAUCzM7UocYCntxrk4xDMvXsmykzjEEs7eubKUOMgT5e1m2VieJb/7RlU3VUeKbfJZlF+os0T19dWXH6jDBFXboRB0ntlWpLNuo80R2We7KrtSBIpt+K8uu1YniuvjelZ2qI8V1XCnLtupMUZ1UuzJThwrqxuvKbtWxYrpyy7I7da6Irv2u7F4dLKLTmrJsp04Wz6KuKxbmqtquzB7U2aJZNpTFwlx219QVC3NZ3liW8UB9YN7cFQvzoaOWsmyvThjHeVtXNlFHDGPU2hUL86dVe1dmhTplDOsuXbEwv6lsyT4W5szbkn0cHzJ3S/axMPtbsk8dVW7TvSsW5pot2feoTqtVtyX7El+Ya7dk31idV2nbr6u0F+aeXSW9MN/2Livd40PzluxbqkOrtGzJvkQX5t1Pukr1F6jWLdm3V+dWaN+SfSkuzF22ZN+TOvrf67Ql+5JbmLttyb7kHqg7bsm+S3X6v9X3UliW2BWxGP1GwWc1AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/8ozirINWdkN7N8AAAAASUVORK5CYII="
+        name: "Vercel",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAAaVBMVEUAAAD///8FBQXw8PBiYmLY2Nj19fX6+vqmpqbm5ubr6+sXFxc9PT3u7u5sbGyPj4+Xl5e5ubnAwMBISEh5eXkhISFOTk7MzMxYWFiIiIgpKSmxsbE1NTXV1dVnZ2eUlJR1dXUwMDCenp4qOBGAAAAB5UlEQVR4nO3b2VLCQBSE4RwkKAZBQFRcEH3/h7TciTnZtMo+cf7vCbr6aqYnyTIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAjnymTjAcY8vVEYbjzGyuzjAUCzM7UocYCntxrk4xDMvXsmykzjEEs7eubKUOMgT5e1m2VieJb/7RlU3VUeKbfJZlF+os0T19dWXH6jDBFXboRB0ntlWpLNuo80R2We7KrtSBIpt+K8uu1YniuvjelZ2qI8V1XCnLtupMUZ1UuzJThwrqxuvKbtWxYrpyy7I7da6Irv2u7F4dLKLTmrJsp04Wz6KuKxbmqtquzB7U2aJZNpTFwlx219QVC3NZ3liW8UB9YN7cFQvzoaOWsmyvThjHeVtXNlFHDGPU2hUL86dVe1dmhTplDOsuXbEwv6lsyT4W5szbkn0cHzJ3S/axMPtbsk8dVW7TvSsW5pot2feoTqtVtyX7El+Ya7dk31idV2nbr6u0F+aeXSW9MN/2Livd40PzluxbqkOrtGzJvkQX5t1Pukr1F6jWLdm3V+dWaN+SfSkuzF22ZN+TOvrf67Ql+5JbmLttyb7kHqg7bsm+S3X6v9X3UliW2BWxGP1GwWc1AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/8ozirINWdkN7N8AAAAASUVORK5CYII=",
       },
       {
         name: "VS Code",
@@ -92,24 +130,20 @@ export const skills = [
       },
       {
         name: "Postman",
-        image:
-          "https://www.svgrepo.com/show/354202/postman-icon.svg",
+        image: "https://www.svgrepo.com/show/354202/postman-icon.svg",
       },
-      {
-        name:"Socket.io",
-        image:"https://socket.io/images/logo-dark.svg"
-      }
     ],
   },
 ];
-  export const experiences = [
+export const experiences = [
   {
     id: 1,
-    img: "https://media.licdn.com/dms/image/v2/D4E0BAQFDjJSMkxWEAQ/company-logo_200_200/company-logo_200_200/0/1718779633884/gnxtace_logo?e=1757548800&v=beta&t=8YZIBsVcNjnCzX398FYXUTHzqvpeRKS4go5oj4_UeyU",
+    img: "/Gnxtace_logo.jpg",
     role: "Software Developer",
     company: "Gnxtace Technologies",
     date: "May 2024 - June 2025",
-desc: "Built a responsive meal delivery web app using React.js and Redux with role-based access for users and admins. Integrated Stripe for payments and REST APIs for orders, subscriptions, and menus. Designed user-friendly UI and developed admin features for managing meals and users.",    skills: [
+    desc: "Built a responsive meal delivery web app using React.js and Redux with role-based access for users and admins. Integrated Stripe for payments and REST APIs for orders, subscriptions, and menus. Designed user-friendly UI and developed admin features for managing meals and users.",
+    skills: [
       "ReactJS",
       "Redux",
       "HTML",
@@ -126,15 +160,8 @@ desc: "Built a responsive meal delivery web app using React.js and Redux with ro
     role: "Software Developer Intern",
     company: "Jnana Inventive Private Limited",
     date: "January 2024 - April 2024",
-desc: "Assisted in building and enhancing web application features, fixing bugs, and improving UI components. Collaborated with senior developers to understand scalable coding practices and contributed to front-end development tasks using React and JavaScript.",
-     skills: [
-      "ReactJS",
-      "Redux",
-      "HTML",
-      "CSS",
-      "JavaScript",
-      "Ant Design",
-    ],
+    desc: "Assisted in building and enhancing web application features, fixing bugs, and improving UI components. Collaborated with senior developers to understand scalable coding practices and contributed to front-end development tasks using React and JavaScript.",
+    skills: ["ReactJS", "Redux", "HTML", "CSS", "JavaScript", "Ant Design"],
     // doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
   },
 ];
@@ -150,7 +177,7 @@ export const education = [
     degree: "Bachelor of Computer Application - BCA ",
   },
   {
-    id: 1, 
+    id: 1,
     img: "https://www.joonsquare.com/usermanage/image/business/schaffter-higher-secondary-school-tirunelveli-10049/schaffter-higher-secondary-school-tirunelveli-logo.jpg",
     school: "Schaffter Higher Secondary School, Tirunelveli",
     date: "Apr 2019 - Apr 2020",
@@ -165,7 +192,7 @@ export const education = [
     date: "Apr 2017 - Apr 2018",
     grade: "86%",
     desc: "I completed my class 10 education at Schaffter Higher Secondary School, Tirunelveli.",
-    degree: "SSLC(X)"
+    degree: "SSLC(X)",
   },
 ];
 
@@ -176,14 +203,8 @@ export const projects = [
     date: "Dec 2023",
     description:
       "Developed weather app using Open Weather API to fetch data, dynamically binding it to create an interactive UI for real-time weather updates.",
-    image:
-      "https://i.ibb.co/DV8zqy2/weatherapp-image.png",
-    tags: [
-      "HTML",
-      "CSS",
-      "Javascript",
-      "React JS",
-    ],
+    image: "https://i.ibb.co/DV8zqy2/weatherapp-image.png",
+    tags: ["HTML", "CSS", "Javascript", "React JS"],
     github: "https://github.com/veeravelS/Weather--app.git",
     webapp: "https://veeravels.github.io/Weather--app/",
   },
@@ -193,9 +214,15 @@ export const projects = [
     date: "Jan 2024",
     description:
       "Developed a Dynamic personal portfolio website to showcase skills,project and professional background ,providing comprehensive overview of expertise in web development .",
-    image:
-      "https://i.ibb.co/k9bh0N0/portfolio-image.png",
-    tags: ["HTML", "CSS", "Javascript", "Bootstrap", "React JS","Styled Components"],
+    image: "https://i.ibb.co/k9bh0N0/portfolio-image.png",
+    tags: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Bootstrap",
+      "React JS",
+      "Styled Components",
+    ],
     github: "https://github.com/veeravelS/Personal-portfolio-Website.git",
     webapp: "personal-portfolio-website-veeravels.vercel.app",
   },
@@ -205,8 +232,7 @@ export const projects = [
     date: "Jan 2024",
     description:
       "This dynamic project enhances user experience by implementing robust client-side validation, ensuring accurate and error-free data submissions. It reflects my skills in front-end development.",
-    image:
-      "https://i.ibb.co/8Xc13XC/Form-validation-image.png",
+    image: "https://i.ibb.co/8Xc13XC/Form-validation-image.png",
     tags: ["HTML", "CSS", "Javascript", "Bootstrap", "jQuery"],
     github: "https://github.com/veeravelS/Form-validation.git",
     webapp: "https://veeravels.github.io/Form-validation/",
@@ -217,9 +243,8 @@ export const projects = [
     date: "May 2024",
     description:
       "This dynamic project enhances user experience by implementing robust client-side validation, ensuring accurate and error-free data submissions. It reflects my skills in front-end development.",
-    image:
-      "https://i.ibb.co/9NxzRPX/currency-convertor.png",
-    tags: ["HTML", "CSS", "Javascript","React"],
+    image: "https://i.ibb.co/9NxzRPX/currency-convertor.png",
+    tags: ["HTML", "CSS", "Javascript", "React"],
     github: "https://github.com/veeravelS/Currency-Convertor.git",
     webapp: "https://veeravels.github.io/Currency-Convertor/",
   },
@@ -229,43 +254,55 @@ export const projects = [
     date: "Oct 2024",
     description:
       "A real-time messaging app with Socket.io, featuring clean UI using Tailwind CSS and Shadcn UI. Includes strong client-side validation and a Node.js + MongoDB backend for seamless communication.",
-    image:
-      "https://i.ibb.co/N2zGK9z9/bg-chatapp.jpg",
-    tags: ["HTML", "CSS", "Javascript", "tailwind CSS", "Shadcn UI", "Socket.io", "Node JS", "Express JS", "MongoDB"],
+    image: "https://i.ibb.co/N2zGK9z9/bg-chatapp.jpg",
+    tags: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "tailwind CSS",
+      "Shadcn UI",
+      "Socket.io",
+      "Node JS",
+      "Express JS",
+      "MongoDB",
+    ],
     github: "https://github.com/veeravelS/Chat-App.git",
     webapp: "https://chat-app-js-dev.vercel.app",
   },
   {
-  id: 6,
-  title: "Online Food Delivery Web App (User & Admin Panel)",
-  date: "June 2025",
-  description:
-    "A fully responsive and scalable web application with both user and admin panels, built using React and Redux. It features dynamic cart management, secure authentication, subscription workflows, and comprehensive admin controls.",
-  image: "https://img.freepik.com/premium-vector/confidential-stamp-confidential-square-sign_822766-10525.jpg",
-  tags: ["HTML", "CSS", "JavaScript", "React", "Redux", "Tailwind CSS"],
-  github: "",
-  webapp: "",
-},
-{
-  id: 7,
-  title: "Mobile App Admin Panel (Content & User Management)",
-  date: "February 2025",
-  description:
-    "Developed a role-based multilingual admin panel for managing users, experts, roles, and content moderation. Features include drag-and-drop user display, banned words filtering, and dynamic configuration based on country and category.",
-  image: "https://img.freepik.com/premium-vector/confidential-stamp-confidential-square-sign_822766-10525.jpg",
-  tags: ["HTML", "CSS", "JavaScript", "React", "Redux"],
-  github: "",
-  webapp: "",
-},
-{
-  id: 8,
-  title: "CMS Admin Panel for Landing Page",
-  date: "December 2024",
-  description:
-    "Built a multilingual CMS admin panel with dynamic modules for managing assets, blogs, SEO metadata, and country-specific content. Enabled non-technical users to update landing page content with ease through a user-friendly interface.",
-  image: "https://img.freepik.com/premium-vector/confidential-stamp-confidential-square-sign_822766-10525.jpg",
-  tags: ["HTML", "Tailwind CSS", "JavaScript", "React", "Redux"],
-  github: "",
-  webapp: "",
-}
+    id: 6,
+    title: "Online Food Delivery Web App (User & Admin Panel)",
+    date: "June 2025",
+    description:
+      "A fully responsive and scalable web application with both user and admin panels, built using React and Redux. It features dynamic cart management, secure authentication, subscription workflows, and comprehensive admin controls.",
+    image:
+      "https://img.freepik.com/premium-vector/confidential-stamp-confidential-square-sign_822766-10525.jpg",
+    tags: ["HTML", "CSS", "JavaScript", "React", "Redux", "Tailwind CSS"],
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 7,
+    title: "Mobile App Admin Panel (Content & User Management)",
+    date: "February 2025",
+    description:
+      "Developed a role-based multilingual admin panel for managing users, experts, roles, and content moderation. Features include drag-and-drop user display, banned words filtering, and dynamic configuration based on country and category.",
+    image:
+      "https://img.freepik.com/premium-vector/confidential-stamp-confidential-square-sign_822766-10525.jpg",
+    tags: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 8,
+    title: "CMS Admin Panel for Landing Page",
+    date: "December 2024",
+    description:
+      "Built a multilingual CMS admin panel with dynamic modules for managing assets, blogs, SEO metadata, and country-specific content. Enabled non-technical users to update landing page content with ease through a user-friendly interface.",
+    image:
+      "https://img.freepik.com/premium-vector/confidential-stamp-confidential-square-sign_822766-10525.jpg",
+    tags: ["HTML", "Tailwind CSS", "JavaScript", "React", "Redux"],
+    github: "",
+    webapp: "",
+  },
 ];
